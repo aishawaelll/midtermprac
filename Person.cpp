ff2a9aa4efad6db103643607004d3d10859b82f1
Aisha <aishawaelll@aucegypt.edu>
2021-03-18 17:16:48 +0200

    added Person.cpp

---
+++ Person.cpp
@@ -0,0 +1,28 @@
+#include "Person.h"
+
+Person::Person()
+{
+	name= " ";
+	age=0;
+}
+
+void Person::setName(string nm)
+{
+	name=nm;
+}
+
+string Person::getName()
+{
+	return name;
+}
+
+void Person::setAge(int a)
+{
+	age=a;
+}
+
+int Person::getName()
+{
+	return age;
+}
+
